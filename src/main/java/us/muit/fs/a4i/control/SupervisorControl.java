0797fd5a10b723722b331cf9cb5a9813324ec9c6
Isabel Román <irm@us.es>
2022-03-09 17:03:17 +0100

    Inicio de persistencia apachepoi
    Se decide realizar la persistencia en documentos excel
    Esta versión incluye un primer acercamiento al manejo de esta librería
    en la clase HSSFReadWrite (que se eliminará posteriormente),
    proporcionada por los proveedores de la api

--- src/main/java/us/muit/fs/a4i/control/SupervisorControl.java
+++ src/main/java/us/muit/fs/a4i/control/SupervisorControl.java
@@ -4,6 +4,8 @@
 package us.muit.fs.a4i.control;
 import org.kohsuke.github.*;
 
+import java.util.HashMap;
+//import java.util.Iterator;
 import java.util.List;
 import java.util.logging.*;
 
@@ -23,8 +25,8 @@ public class SupervisorControl {
 			try {
 			GitHub github = GitHubBuilder.fromEnvironment().build();
 			GHMyself myinfo=github.getMyself();
-			GHRepository unrepo=github.getRepository("MIT-FS/ShopManager");
-			PagedIterable<GHRepository> myrepos=myinfo.listRepositories();
+			//GHRepository unrepo=github.getRepository("MIT-FS/ShopManager");
+			//PagedIterable<GHRepository> myrepos=myinfo.listRepositories();
 			PagedIterable<GHRepository> myOwnRepos=myinfo.listRepositories(10, GHMyself.RepositoryListFilter.OWNER);
 			int count=1;
 			for(GHRepository repo:myOwnRepos.toList()) {
@@ -42,11 +44,55 @@ public class SupervisorControl {
 				}
 				count++;
 			}
-
+            GHPersonSet<GHOrganization> misOrganizaciones = myinfo.getAllOrganizations();
+            System.out.println("Pertenezco a las siguientes organizaciones: ");
+            //Iterator<GHOrganization> iteradorOrganizaciones = misOrganizaciones.iterator();
+            int i=1;
+            for(GHOrganization organizacion: misOrganizaciones) {
+            	System.out.println(i+" Organización "+organizacion.getId()+" : "+organizacion);
+            	PagedIterable<GHRepository> repos=organizacion.listRepositories();
+            	System.out.println(repos);
+            	i++;
+            }
+            /*
 			log.info("Mis datos "+myinfo);		
-			log.info("Mi repositorio "+unrepo);
+			log.info("Un repositorio "+unrepo);
 			log.info("Número de repositorios "+myrepos.toList().size());
 			log.info("Detalles de mis repositorios "+myrepos.toList());
+			*/
+            GHOrganization unaOrg = github.getOrganization("MIT-FS");
+          //  PagedIterable<GHRepository> repos=unaOrg.listRepositories();
+        	System.out.println("Recupero la organización "+unaOrg.getId());
+        	GHRepository githubrepo=github.getRepository("hub4j/github-api");
+        	System.out.println("Este repositorio es de "+githubrepo.getOwnerName()+" Y su descripción es "+githubrepo.getDescription());
+        	GHRepositoryStatistics estadisticas=githubrepo.getStatistics();
+        	log.info("Estadisticas recogidas");
+        	  	
+        //	List<GHProject> proyectos=githubrepo.listProjects().toList();
+        	PagedIterable<GHRepositoryStatistics.ContributorStats> estDes=estadisticas.getContributorStats();
+        	log.info("Desarrolladores recogidos");
+        	List<GHRepositoryStatistics.ContributorStats> listaDesarrolladores=estDes.toList();
+        	System.out.println("Número de desarrolladores "+listaDesarrolladores.size());
+        	      	    	
+        	i=1;
+
+
+            HashMap <String, GHRepositoryStatistics.ContributorStats> mapaEstadisticasUsuario = new HashMap <String, GHRepositoryStatistics.ContributorStats> ();
+        	for (GHRepositoryStatistics.ContributorStats desarrollador:listaDesarrolladores) {
+        		//System.out.println(i+" Desarrollador "+desarrollador.getAuthor().getName()+" mail "+desarrollador.getAuthor().getEmail()+ " login "+desarrollador.getAuthor().getLogin());
+//        		GHUser usuario=github.getUser(desarrollador.getAuthor().getLogin());
+        		mapaEstadisticasUsuario.put(desarrollador.getAuthor().getLogin(), desarrollador);
+        		i++;
+        	}
+        	System.out.println("Datos del usuario rtyley "+mapaEstadisticasUsuario.get("rtyley"));
+        	System.out.println("Semanas "+mapaEstadisticasUsuario.get("rtyley").getWeeks());
+        	
+			/*for(GHProject project:proyectos){
+				System.out.println("Con proyecto "+i+" llamado "+project.getName()+" con id "+project.getId());
+				GHProject audit= github.getProject(project.getId());
+				System.out.println(audit);
+				i++;
+			}*/
 		}catch(Exception e) {
 				log.info(e+" No se puede crear la instancia GitHub\n");
 				log.info("Recuerde que debe configurar las variables de entorno GITHUB_LOGIN y GITHUB_OAUTH con su nombre de usuario y token respectivamente");
